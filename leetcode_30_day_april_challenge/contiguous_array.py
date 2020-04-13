b2a0ca12490412890a067a196935b10fdfd45092
Sim <simeon@Sims-MacBook-Pro.local>
2020-04-14 00:39:05 +0100

    contiguous array

---
+++ leetcode_30_day_april_challenge/contiguous_array.py
@@ -0,0 +1,21 @@
+"""Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1."""
+
+from typing import List
+
+def findMaxLength(nums: List[int]) -> int:
+        counts: dict = {0: -1}
+        max_length: int = 0
+        count: int = 0
+            
+        for i in range(len(nums)):
+            if not nums[i]:
+                count -= 1
+            else:
+                count += 1
+            
+            if count in counts:
+                max_length: int = max(max_length, i - counts.get(count))
+            else:
+                counts[count]: int = i
+        
+        return max_length
