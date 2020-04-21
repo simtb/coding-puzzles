9a7de14b241490ca73af3af66c49bc8c7bf124e0
Sim <simeon@Sims-MacBook-Pro.local>
2020-04-21 21:29:15 +0100

    left column with at least a one

---
+++ leetcode_30_day_april_challenge/leftmost_column_with_at_least_a_one.py
@@ -0,0 +1,59 @@
+"""
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix, this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix interface:
+
+BinaryMatrix.get(x, y) returns the element of the matrix at index (x, y) (0-indexed).
+BinaryMatrix.dimensions() returns a list of 2 elements [n, m], which means the matrix is n * m.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples. You will not have access the binary matrix directly.
+"""
+
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    def _binarySearch(self, binaryMatrix: 'BinaryMatrix', y: int) -> int:
+        left: int = 0
+        right: int = binaryMatrix.dimensions()[1] - 1
+        smallest_index: int = None
+        
+        while left <= right:
+            mid: int = left + ((right - left) // 2)
+        
+            if binaryMatrix.get(y, mid) == 1:
+                smallest_index: int = mid
+                right: int = mid - 1
+            else:
+                left: int = mid + 1
+        
+        if smallest_index is not None:
+            return smallest_index
+        return None
+        
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        if not binaryMatrix.dimensions()[0] or  not binaryMatrix.dimensions()[1]:
+            return -1
+        
+        first_column: int = 100
+            
+        for i in range(binaryMatrix.dimensions()[0]):
+            current_column: int = self._binarySearch(binaryMatrix, i)
+            if current_column is not None:
+                if current_column == 0:
+                    return current_column
+                else:
+                    first_column: int = min(first_column, current_column)
+        
+        if first_column == 100:
+            return -1
+        else:
+            return first_column
